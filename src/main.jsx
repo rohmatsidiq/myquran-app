2782a9deaf8fd02e2e70610e68445cc2b4679b0a
Muslimafiyah Publishing <muslimafiyahpublishing@pop-os.localdomain>
2023-11-13 08:30:02 +0700

    edit html manifest

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-ServiceWorker.unregister();
-
 ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
         <App />
