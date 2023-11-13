62cb3c985485849e5fb4091b2fc46ba90084e054
Muslimafiyah Publishing <muslimafiyahpublishing@pop-os.localdomain>
2023-11-13 08:49:08 +0700

    edit html manifest

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/detail/:nomor" element={<Detail />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/detail/:nomor" element={<Detail />} />
+        </Routes>
+    );
 }
 
 export default App;
