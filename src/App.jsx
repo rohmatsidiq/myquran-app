1240eab4cc499b17122a083a3760f59189a04ed8
Rohmat Sidiq Mustaqim <rohmatsidiq9@gmail.com>
2024-05-16 10:31:46 +0700

    membuat fitur back agar keluat app

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,7 @@ import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 
 function App() {
-    useEffect(() => {
-        const handleBackButton = (event) => {
-            if (event.currentTarget.performance.navigation.type === 1) {
-                // Close the PWA app when user navigates back from another page
-                window.close();
-            }
-        };
 
-        // Add event listener to window.history for detecting back navigation
-        window.history.pushState(null, null, window.location.pathname);
-        window.addEventListener('popstate', handleBackButton);
-
-        return () => {
-            window.removeEventListener('popstate', handleBackButton);
-        };
-    }, []);
     return (
         <Routes>
             <Route path="/" element={<Home />} />
