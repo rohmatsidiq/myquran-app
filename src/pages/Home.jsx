944285fad8bd904cf507da73f12776641ccb14a8
Rohmat Sidiq Mustaqim <rohmatsidiq9@gmail.com>
2024-09-20 13:15:01 +0700

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,11 +8,25 @@ import Hero from "../components/Hero";
 export default function Home() {
   const [surat, setSurat] = useState([]);
   const [cari, setCari] = useState("");
+  const [suratFiltered, setSuratFiltered] = useState([]);
 
   const getSurat = async () => {
     const response = await axios("https://equran.id/api/v2/surat");
     setSurat(response.data.data);
+    setSuratFiltered(response.data.data);
   };
+
+  useEffect(() => {
+    setSuratFiltered(
+      cari == ""
+        ? surat
+        : surat.filter(
+            (f) =>
+              f.namaLatin.toLowerCase().includes(cari) ||
+              f.arti.toLowerCase().includes(cari)
+          )
+    );
+  }, [cari]);
 
   useEffect(() => {
     getSurat();
@@ -29,61 +43,30 @@ export default function Home() {
           placeholder="Cari"
         />
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3">
-          {cari != ""
-            ? surat
-                .filter(
-                  (f) =>
-                    f.namaLatin.toLowerCase().includes(cari) ||
-                    f.arti.toLowerCase().includes(cari)
-                )
-                .map((e) => (
-                  <Link
-                    key={e.nomor}
-                    to={`/detail/${e.nomor}`}
-                    className="bg-white hover:shadow-xl hover:shadow-[#fff6f1] rounded-3xl p-4"
-                  >
-                    <div className="flex justify-between">
-                      <div>
-                        <div className="flex gap-3 items-center">
-                          <div className="w-10 h-10 border-2 border-[#b3856b]">
-                            <div className="w-10 h-10 border-2 border-[#b3856b] rotate-45 flex justify-center items-center">
-                              <p className="-rotate-45 font-bold">{e.nomor}</p>
-                            </div>
-                          </div>
-                          <div>
-                            <h1 className="font-bold">{e.namaLatin}</h1>
-                            <p>({e.arti})</p>
-                          </div>
-                        </div>
+          {suratFiltered.map((e) => (
+            <Link
+              key={e.nomor}
+              to={`/detail/${e.nomor}`}
+              className="bg-white hover:shadow-xl hover:shadow-[#fff6f1] rounded-3xl p-4"
+            >
+              <div className="flex justify-between">
+                <div>
+                  <div className="flex gap-3 items-center">
+                    <div className="w-10 h-10 border-2 border-[#b3856b]">
+                      <div className="w-10 h-10 border-2 border-[#b3856b] rotate-45 flex justify-center items-center">
+                        <p className="-rotate-45 font-bold">{e.nomor}</p>
                       </div>
-                      <h1 className="font-arab text-3xl">{e.nama}</h1>
                     </div>
-                  </Link>
-                ))
-            : surat.map((e) => (
-                <Link
-                  key={e.nomor}
-                  to={`/detail/${e.nomor}`}
-                  className="bg-white hover:shadow-xl hover:shadow-[#fff6f1] rounded-3xl p-4"
-                >
-                  <div className="flex justify-between">
                     <div>
-                      <div className="flex gap-3 items-center">
-                        <div className="w-10 h-10 border-2 border-[#b3856b]">
-                          <div className="w-10 h-10 border-2 border-[#b3856b] rotate-45 flex justify-center items-center">
-                            <p className="-rotate-45 font-bold">{e.nomor}</p>
-                          </div>
-                        </div>
-                        <div>
-                          <h1 className="font-bold">{e.namaLatin}</h1>
-                          <small>({e.arti})</small>
-                        </div>
-                      </div>
+                      <h1 className="font-bold">{e.namaLatin}</h1>
+                      <p>({e.arti})</p>
                     </div>
-                    <h1 className="font-arab text-3xl">{e.nama}</h1>
                   </div>
-                </Link>
-              ))}
+                </div>
+                <h1 className="font-arab text-3xl">{e.nama}</h1>
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
     </>
