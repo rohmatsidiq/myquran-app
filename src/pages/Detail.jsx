dee94f493a7809dc98c2e1f9933696dbc7da81c3
Rohmat Sidiq Mustaqim <rohmatsidiq9@gmail.com>
2026-03-11 07:16:38 +0700

    update

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -145,7 +145,7 @@ export default function Detail() {
                   {e.teksArab}
                 </p>
 
-                <div className="bg-gray-50/50 p-6 rounded-[2rem] border-l-4 border-orange-400 mb-8">
+                <div className="bg-gray-50/50 p-6 rounded-3xl border-l-4 border-orange-400 mb-8">
                   <p className="text-gray-600 text-base md:text-lg leading-relaxed font-medium">
                     {e.teksIndonesia}
                   </p>
